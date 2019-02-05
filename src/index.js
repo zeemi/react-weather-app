8c3e9d326b0c6d68b36e7bbdd434a545caf66fd5
Marcin Siedlarz <marcin.siedlarz@joymile.com>
2019-02-05 19:49:47 +0100

    first react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello there!')
+import ReactDom from 'react-dom';
+import React from 'react';
+import App from "./components/App";
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+ReactDom.render(<App/>, document.getElementById('root'))
